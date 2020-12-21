05d1b9af6ba04ff95362f5152f99ff2a1dd9a826
serghalak <serghalak@gmail.com>
2020-12-21 17:10:55 +0200

    add dao to customer controller

--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -1,15 +1,31 @@
 package com.luv2code.springdemo.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.luv2code.springdemo.dao.CustomerDao;
+import com.luv2code.springdemo.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
+	private CustomerDao customerDao;	
+	
+	public CustomerController(CustomerDao customerDao) {		
+		this.customerDao = customerDao;
+	}
+
+
 	@RequestMapping("/list")
-	public String listCustomers(Model theModel) {
+	public String listCustomers(Model theModel) {		
+		List<Customer>listCustomer=customerDao.getCustomers();
+		
+		theModel.addAttribute("customers",listCustomer);
+		System.out.println(listCustomer);
 		return "list-customers";
 	}
 
